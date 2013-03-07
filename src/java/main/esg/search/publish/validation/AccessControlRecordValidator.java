401bdc070383b9066d0feb1f0d33f7da098b7030
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2013-03-07 04:58:46 -0700

    Inserting auto,atic reloading for access control configuration.

--- src/java/main/esg/search/publish/validation/AccessControlRecordValidator.java
+++ src/java/main/esg/search/publish/validation/AccessControlRecordValidator.java
@@ -16,13 +16,14 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.Namespace;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.StringUtils;
 
 import esg.common.util.ESGFProperties;
 import esg.search.core.Record;
 import esg.search.query.api.QueryParameters;
 import esg.search.utils.XmlParser;
+import esg.security.registry.service.api.ReloadableFileSetObserver;
+import esg.security.registry.service.impl.ReloadableFileSet;
 
 /**
  * Special {@link RecordValidator} that enforces restrictions
@@ -31,11 +32,8 @@ import esg.search.utils.XmlParser;
  * @author Luca Cinquini
  *
  */
-public class AccessControlRecordValidator implements RecordValidator {
+public class AccessControlRecordValidator implements RecordValidator, ReloadableFileSetObserver {
     
-    // location of configuration schema
-    //private final String FILEPATH = "esg/search/config/esgf_project_restrictions.xml";
-    private String filepath;
     
     private final Log LOG = LogFactory.getLog(this.getClass());
     
@@ -49,13 +47,18 @@ public class AccessControlRecordValidator implements RecordValidator {
     
     // map containing entries of the form (project, index_node, pattern)
     private MultiKeyMap cache = new MultiKeyMap();
+    
+    // Utility class that watches the set of local XML configuration files for changes
+    private ReloadableFileSet watcher;
 
     public AccessControlRecordValidator(String filepath, ESGFProperties properties) throws Exception {
+               
+        // instantiate files watcher
+        watcher = new ReloadableFileSet(filepath);
+        watcher.setObserver(this);
         
-        // parse restrictions from XML file
-        this.filepath = filepath;
-        // FIXME: load only if file changes
-        parseConfig();
+        // trigger first loading of configuration files
+        watcher.reload();
         
         // load local hostname
         if (properties.containsKey(PROPERTIES_KEY)) {
@@ -69,7 +72,7 @@ public class AccessControlRecordValidator implements RecordValidator {
 
     @Override
     public void validate(Record record, List<String> errors) throws Exception {
-        
+                
         String type = record.getType();
         String id = record.getId();
         
@@ -114,46 +117,68 @@ public class AccessControlRecordValidator implements RecordValidator {
     /**
      * Method to load the publication restrictions from the XML configuration file.
      */
-    private void parseConfig() throws IOException, JDOMException {
+    public void parse(List<File> files) {
         
-        File file = new ClassPathResource(filepath).getFile();
-        if (LOG.isInfoEnabled()) LOG.info("Parsing XML config file: "+file.getAbsolutePath());
+        // temporary stores for access control policies
+        MultiKeyMap _cache = new MultiKeyMap();
+        Set<String> _projects = new HashSet<String>();
         
-        // parse XML 
-        XmlParser parser = new XmlParser(false); // validate XSD = false
-        Document doc = parser.parseFile(file);
-        Element root = doc.getRootElement();
-        Namespace ns = root.getNamespace();
-        
-        /*
-         * <esgf:project name="obs4MIPs">
-                <esgf:index_node hostname="localhost">
-                    <esgf:datasets pattern=".*"/>
-                </esgf:index_node>
-           </esgf:project>
-         */          
-        for (Object obj1 : root.getChildren("project", ns)) {
-            Element el1 = (Element)obj1;
-            String project = el1.getAttributeValue("name");
-            projects.add(project);
+        // loop over configuration files
+        for (File file : files) {
+            
+            try {    
+                
+                if (LOG.isInfoEnabled()) LOG.info("Parsing XML config file: "+file.getAbsolutePath());
+                
+                // parse XML 
+                XmlParser parser = new XmlParser(false); // validate XSD = false
+                Document doc = parser.parseFile(file);
+                Element root = doc.getRootElement();
+                Namespace ns = root.getNamespace();
+                
+                /*
+                 * <esgf:project name="obs4MIPs">
+                        <esgf:index_node hostname="localhost">
+                            <esgf:datasets pattern=".*"/>
+                        </esgf:index_node>
+                   </esgf:project>
+                 */          
+                for (Object obj1 : root.getChildren("project", ns)) {
+                    Element el1 = (Element)obj1;
+                    String project = el1.getAttributeValue("name");
+                    _projects.add(project);
+                                
+                    for (Object obj2 : el1.getChildren("index_node", ns)) {
+                        Element el2 = (Element)obj2;
+                        String indexNode = el2.getAttributeValue("hostname");
                         
-            for (Object obj2 : el1.getChildren("index_node", ns)) {
-                Element el2 = (Element)obj2;
-                String indexNode = el2.getAttributeValue("hostname");
-                
-                List<Pattern> patterns = new ArrayList<Pattern>();
-                for (Object obj3 : el2.getChildren("datasets", ns)) {
-                    Element el3 = (Element)obj3;
+                        List<Pattern> patterns = new ArrayList<Pattern>();
+                        for (Object obj3 : el2.getChildren("datasets", ns)) {
+                            Element el3 = (Element)obj3;
+                            
+                            String pattern = el3.getAttributeValue("pattern");
+                            if (LOG.isInfoEnabled()) LOG.info("Restriction: project="+project+" index_node="+indexNode+" pattern="+pattern);
+                            patterns.add(Pattern.compile(pattern));
+                            
+                        }               
+                        _cache.put(project, indexNode, patterns);
+                        
+                    }
                     
-                    String pattern = el3.getAttributeValue("pattern");
-                    if (LOG.isInfoEnabled()) LOG.info("Restriction: project="+project+" index_node="+indexNode+" pattern="+pattern);
-                    patterns.add(Pattern.compile(pattern));
-                    
-                }               
-                cache.put(project, indexNode, patterns);
-                
+                }
+            
+            } catch(IOException ioe) {
+                LOG.warn(ioe.getMessage());
+            } catch(JDOMException jde) {
+                LOG.warn(jde.getMessage());
             }
             
+        } // loop over files
+        
+        // update local data storage
+        synchronized (cache) {
+            projects = _projects;
+            cache = _cache;
         }
         
     }
