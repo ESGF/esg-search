1592033509ef24bc2907dc841cbeb9dc94f95009
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2013-03-19 12:21:39 -0600

    Generalizing the search reporter to arbitrary facets.

--- src/java/main/esg/search/metrics/SearchReporter.java
+++ src/java/main/esg/search/metrics/SearchReporter.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.xpath.XPath;
@@ -13,30 +14,40 @@ import esg.search.utils.XmlParser;
 
 public class SearchReporter extends AbstractReporter {
         
-    public final static String XPATH1 = "/response/result";
-    private final XPath xpath1;
+    public final static String XPATH = "/response/result";
+    private final XPath xpath;
     
-    public final static String XPATH2 = "/response/lst[@name='facet_counts']/lst[@name='facet_fields']/lst[@name='model']";
-    private final XPath xpath2;
+    String[] facets;
     
-    public final static String XPATH3 = "/response/lst[@name='facet_counts']/lst[@name='facet_fields']/lst[@name='project']";
-    private final XPath xpath3;
+    // Example: XPATH_TEMPLATE = "/response/lst[@name='facet_counts']/lst[@name='facet_fields']/lst[@name='project']";
+    public final static String XPATH_TEMPLATE = "/response/lst[@name='facet_counts']/lst[@name='facet_fields']/lst[@name='FACET']";
+    private final XPath[] xpaths;
     
-    private String url = "http://INDEX_NODE_HOSTNAME/esg-search/search?latest=true&replica=false&facets=model,project";
-    private String cvs_file_path = "/esg/content/metrics/search_INDEX_NODE_HOSTNAME.csv";
-    private String xml_file_path = "/esg/content/metrics/search_INDEX_NODE_HOSTNAME.xml";
+    // Example: url = "http://INDEX_NODE_HOSTNAME/esg-search/search?latest=true&replica=false&facets=model,project";
+    private String url = "http://INDEX_NODE_HOSTNAME/esg-search/search?latest=true&replica=false&facets=FACETS";
+    
+    private String cvs_file_path = "/esg/content/metrics/search_INDEX_NODE_HOSTNAME_FACETS.csv";
+    private String xml_file_path = "/esg/content/metrics/search_INDEX_NODE_HOSTNAME_FACETS.xml";
     
     private final static String REPORT_TYPE = "Search Results";
     
-    public SearchReporter(String hostname) throws Exception {
+    public SearchReporter(String hostname, String[] facets) throws Exception {
         
-        url = url.replaceAll("INDEX_NODE_HOSTNAME", hostname);
-        cvs_file_path = cvs_file_path.replaceAll("INDEX_NODE_HOSTNAME", hostname);
-        xml_file_path = xml_file_path.replaceAll("INDEX_NODE_HOSTNAME", hostname);
+        url = url.replaceAll("INDEX_NODE_HOSTNAME", hostname)
+                 .replaceAll("FACETS", StringUtils.join(facets, ","));
         
-        xpath1 = XPath.newInstance(XPATH1);
-        xpath2 = XPath.newInstance(XPATH2);
-        xpath3 = XPath.newInstance(XPATH3);
+        cvs_file_path = cvs_file_path.replaceAll("INDEX_NODE_HOSTNAME", hostname)
+                                     .replaceAll("FACETS", StringUtils.join(facets, "_"));
+        xml_file_path = xml_file_path.replaceAll("INDEX_NODE_HOSTNAME", hostname)
+                                     .replaceAll("FACETS", StringUtils.join(facets, "_"));
+        
+        xpath = XPath.newInstance(XPATH);
+
+        this.facets = facets;
+        xpaths = new XPath[facets.length];
+        for (int i=0; i<facets.length; i++) {
+            xpaths[i] = XPath.newInstance( XPATH_TEMPLATE.replaceAll("FACET", facets[i]) );
+        }
         
     }
 
@@ -45,13 +56,15 @@ public class SearchReporter extends AbstractReporter {
      */
     public static void main(String[] args) throws Exception {
         
-        if (args.length!=1) {
-            System.out.println("Usage: java esg.search.metrics <index.node.hostname>");
+        if (args.length<=1) {
+            System.out.println("Usage: java esg.search.metrics <index.node.hostname> <facet_1> <facet_2> <facet_3>....");
             System.exit(-1);
         }
         String indexNode = args[0];
+        String[] facets = new String[args.length-1];
+        for (int i=0; i<args.length-1; i++) facets[i] = args[i+1];
         
-        SearchReporter self = new SearchReporter(indexNode);
+        SearchReporter self = new SearchReporter(indexNode, facets);
         self.run();
 
     }
@@ -69,30 +82,21 @@ public class SearchReporter extends AbstractReporter {
         Map<String, Integer> map = new HashMap<String, Integer>();
         
         // <result name="response" numFound="9" start="0" maxScore="1.0">
-        Element resultElement = (Element)xpath1.selectSingleNode(doc);
+        Element resultElement = (Element)xpath.selectSingleNode(doc);
         int total = Integer.parseInt( resultElement.getAttributeValue("numFound") );
         map.put("Total", total);
         
         //  <lst name="model">
         //      <int name="CCSM4">2237</int>
         //      <int name="CESM1(BGC)">188</int>
-        Element modelElement = (Element)xpath2.selectSingleNode(doc);
-        for (Object obj : modelElement.getChildren("int")) {
-            Element elem = (Element)obj;
-            String name = elem.getAttributeValue("name");
-            int counts = Integer.parseInt(elem.getTextNormalize());
-            map.put("model="+name, counts);
-        }
-        
-        //  <lst name="project">
-        //      <int name="CMIP5">11195</int>
-        //      <int name="GeoMIP">169</int>
-        Element projectElement = (Element)xpath3.selectSingleNode(doc);
-        for (Object obj : projectElement.getChildren("int")) {
-            Element elem = (Element)obj;
-            String name = elem.getAttributeValue("name");
-            int counts = Integer.parseInt(elem.getTextNormalize());
-            map.put("project="+name, counts);
+        for (int i=0; i<facets.length; i++) {
+            Element facetElement = (Element)xpaths[i].selectSingleNode(doc);
+            for (Object obj : facetElement.getChildren("int")) {
+                Element elem = (Element)obj;
+                String name = elem.getAttributeValue("name");
+                int counts = Integer.parseInt(elem.getTextNormalize());
+                map.put(facets[i]+"="+name, counts);
+            }
         }
 
         return map;
