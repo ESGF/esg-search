b4c11262067f5f86ff1fdad584d7c8677115cd85
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2014-07-14 07:10:17 -0600

    Changing metadata field "data_format" to "format".

--- src/java/main/esg/search/query/impl/solr/SolrXmlPars.java
+++ src/java/main/esg/search/query/impl/solr/SolrXmlPars.java
@@ -112,7 +112,7 @@ public class SolrXmlPars {
 	final public static String FIELD_HEIGHT_UNITS = "height_units";
 	final public static String FIELD_GEO = "geo";
 	
-	final public static String FIELD_DATA_FORMAT = "data_format";
+	final public static String FIELD_DATA_FORMAT = "format";
 	final public static String FIELD_FILE_NAME = "file_name";
 			
 	/**
