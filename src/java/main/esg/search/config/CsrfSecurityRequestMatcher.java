1c2ddcaeaa71f943c14f1a4867fbca979f446d48
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-06-24 04:38:46 -0600

    Customizing the Spring Security http filter chain.

---
+++ src/java/main/esg/search/config/CsrfSecurityRequestMatcher.java
@@ -0,0 +1,28 @@
+package esg.search.config;
+
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+public class CsrfSecurityRequestMatcher implements RequestMatcher {
+    
+	private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+    private RegexRequestMatcher unprotectedMatcher = new RegexRequestMatcher("/**", null);
+ 
+    @Override
+    public boolean matches(HttpServletRequest request) {
+    	
+    	// exclude GET|HEAD|TRACE|OPTIONS from CSRF validation
+        if (allowedMethods.matcher(request.getMethod()).matches()) {
+            return false;
+        }
+ 
+        // exclude special URLs from CSRF validation
+        // enforce CSRF validation on all others
+        return !unprotectedMatcher.matches(request);
+        
+    }
+}
