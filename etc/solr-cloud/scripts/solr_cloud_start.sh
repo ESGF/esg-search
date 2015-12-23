96c7b6c1b729b9e7e1143b65da09d49ccf78d98a
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-12-23 06:47:10 -0700

    Giving solr more memory.

--- etc/solr-cloud/scripts/solr_cloud_start.sh
+++ etc/solr-cloud/scripts/solr_cloud_start.sh
@@ -3,6 +3,6 @@
 
 cd $SOLR_CLOUD_HOME
 
-solr/bin/solr start -c -p 8983 -s node8983/solr/
-solr/bin/solr start -c -p 8984 -s node8984/solr/ -z localhost:9983
-solr/bin/solr start -c -p 8985 -s node8985/solr/ -z localhost:9983
+solr/bin/solr start -c -p 8983 -s node8983/solr/  -m 1024m
+solr/bin/solr start -c -p 8984 -s node8984/solr/ -z localhost:9983 -m 1024m
+solr/bin/solr start -c -p 8985 -s node8985/solr/ -z localhost:9983 -m 1024m
