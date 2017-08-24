f3dfdea36eef480f24aaa73c4514c035923b688f
Luca Cinquini <cinquini@LMC-038261.local>
2017-08-24 07:06:07 -0600

    Fixing problem with converting longitudes to -180, 180 range.

--- src/java/main/esg/search/utils/GeoUtils.java
+++ src/java/main/esg/search/utils/GeoUtils.java
@@ -3,36 +3,57 @@ package esg.search.utils;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class GeoUtils {
+	
+	private static final Log LOG = LogFactory.getLog(GeoUtils.class);
 	
     public static List<float[]> convertLongitudeRangeto180(float lonMin, float lonMax) {
         
     	List<float[]> lonRanges = new ArrayList<float[]>();
     	
-    	// return left interval
-    	if (lonMax <= 180) {
-    		float[] lonRange = new float[]{ lonMin, lonMax };
-    		lonRanges.add(lonRange);
-    		
-    	// return right interval, shifted
-    	} else if (lonMin >= 180) {
-    		float[] lonRange = new float[]{ lonMin-360, lonMax-360 };
-    		lonRanges.add(lonRange);
-    		
-    	// must split into two intervals
-    	} else {
-    		
-    		float[] lonRange1 = new float[]{ -180, lonMax-360 };
-    		float[] lonRange2 = new float[]{ lonMin, 180 };
-    		lonRanges.add(lonRange1);
-    		lonRanges.add(lonRange2);
-    		
+    	// invalid longitude values
+    	if ( (lonMax < lonMin) || (lonMax > lonMin + 360) ) {
+    		LOG.warn("Invalid longitude values: lonMin="+lonMin+" lonMax="+lonMax);
+    		lonRanges.add( new float[]{ 0, 0 } ); 
+    		return lonRanges;
     	}
     	
+    	while (lonMin < 180) {
+    		lonMin += 360;
+    		lonMax += 360;
+    	}
+    	
+    	while (lonMin >= 180) {
+    		lonMin -= 360;
+    		lonMax -= 360;
+    	}
+    	
+        if (lonMax >= 180) {
+        	
+        	lonRanges.add( new float[]{ -180, lonMax-360 } );
+        	lonRanges.add( new float[]{ lonMin, 180 } );
+
+        } else {
+        	
+        	lonRanges.add( new float[]{ lonMin, lonMax } );
+        }
+    		    	
     	return lonRanges;
     	
     }
 	
 	private GeoUtils() {}
+	
+	public static void main(String[] args) {
+		
+		 List<float[]> lonRanges = GeoUtils.convertLongitudeRangeto180( -279.5f,  79.5f);
+		 for (float[] lonRange : lonRanges) {
+			 System.out.println(lonRange[0]+", "+lonRange[1]);
+		 }
+		
+	}
 
 }
